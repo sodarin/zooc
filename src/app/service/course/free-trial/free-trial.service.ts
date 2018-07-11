ebb6c3a7d8ae6f408206da877357dca57a103bef
Zenas Chen <czyczk@qq.com>
2018-07-11 23:29:49 +0800

    trivial

--- src/app/service/course/free-trial/free-trial.service.ts
+++ src/app/service/course/free-trial/free-trial.service.ts
@@ -7,10 +7,10 @@ import {FreeTrial} from '../../../model/FreeTrialModel';
 export class FreeTrialService {
 
   freeTrials = [
-    new FreeTrial('1','../../../assets/img/free-trial/5a39cd3f0001c09805400300.jpg', '全网最热Python3入门', 336.00, '初级', 510),
-    new FreeTrial('2','../../../assets/img/free-trial/5ac2dfe100014a9005400300.jpg', '分布式事务实践', 348.00, '高级', 130),
-    new FreeTrial('3','../../../assets/img/free-trial/5b2a29d50001bf4605400300.jpg', 'Python3入门机器学习 经典算法与应用', 400.00, '初级', 600),
-    new FreeTrial('4','../../../assets/img/free-trial/59b8a486000107fb05400300.jpg', 'Vue2.5开发从零基础入门到实战项目', 226.00, '中级', 410)
+    new FreeTrial('1', '../../../assets/img/free-trial/5a39cd3f0001c09805400300.jpg', '全网最热Python3入门', 336.00, '初级', 510),
+    new FreeTrial('2', '../../../assets/img/free-trial/5ac2dfe100014a9005400300.jpg', '分布式事务实践', 348.00, '高级', 130),
+    new FreeTrial('3', '../../../assets/img/free-trial/5b2a29d50001bf4605400300.jpg', 'Python3入门机器学习 经典算法与应用', 400.00, '初级', 600),
+    new FreeTrial('4', '../../../assets/img/free-trial/59b8a486000107fb05400300.jpg', 'Vue2.5开发从零基础入门到实战项目', 226.00, '中级', 410)
   ];
 
   constructor() { }
