0e24f2cea3815b3d5cea05b28b19719ed823572e
Zenas Chen <czyczk@qq.com>
2018-07-15 22:00:28 +0800

    Refined user page

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -5,7 +5,7 @@ import {LoginService, UserResponse} from '../service/login/login.service';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
 
