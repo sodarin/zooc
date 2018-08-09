71f4963cecb0c2d7fa515e04b299a7dbcc888228
Zenas Chen <czyczk@qq.com>
2018-08-09 09:38:49 +0800

    Bug fix: Incorrect relative path

--- src/app/core/carousel/carousel.component.ts
+++ src/app/core/carousel/carousel.component.ts
@@ -8,10 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class CarouselComponent implements OnInit {
 
   pictures = [
-    {'src': '../../assets/img/carousel/5afcec3a0001528009360316.jpg'},
-    {'src': '../../assets/img/carousel/5b1e47160001d8a918720632.jpg'},
-    {'src': '../../assets/img/carousel/5b2b81bb00016aee18720632.jpg'},
-    {'src': '../../assets/img/carousel/5b174e000001db4809360316.jpg'}
+    {'src': '../../../assets/img/carousel/5afcec3a0001528009360316.jpg'},
+    {'src': '../../../assets/img/carousel/5b1e47160001d8a918720632.jpg'},
+    {'src': '../../../assets/img/carousel/5b2b81bb00016aee18720632.jpg'},
+    {'src': '../../../assets/img/carousel/5b174e000001db4809360316.jpg'}
   ];
 
   constructor() { }
